6a79931a23c15fa6c1cea02596a6f999173dd6b4
Benson <36316652+Jinb2@users.noreply.github.com>
2021-09-01 15:28:08 -0400

    Started about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar";
 
 const About = () => {
-    return(
-        <div>
-            <Navbar/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <h1>What is climate change</h1>
+      <div className="definitions">Evidence</div>
+      <div className="definitions">Causes</div>
+      <div className="definitions">Effects</div>
+      <div className="definitions">Solutions</div>
+    </div>
+  );
+};
 
 export default About;
