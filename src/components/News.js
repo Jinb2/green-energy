09631e85f860e0734662a220f59194662694d3c5
Benson <36316652+Jinb2@users.noreply.github.com>
2021-09-22 00:25:40 -0400

    Setup up fetch and created Article component

--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Article from "./Article";
+import Navbar from "./Navbar";
 
 const News = () => {
-    return(
-        <div>
-        </div>
-    )
-}
+  const [news, setNews] = useState({});
+  const api_key = "f9234e69e30e483ebed9f7909613092a"; // for news api integration
+
+  // fetch data from this url
+  async function fetchNewsJSON() {
+    // wait for promise
+    const response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + api_key
+    );
+    const json = await response.json();
+    setNews(json);
+  }
+
+  // controls the number of calls on fetch function
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchNewsJSON();
+    }, 86400000); // run every day
+    return () => clearInterval(interval); // prevent memory leak
+  }, []);
+
+  return (
+    <div className="news">
+      <Navbar />
+      <Article news={news} />
+    </div>
+  );
+};
 export default News;
